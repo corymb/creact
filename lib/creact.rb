0e1ffd05322ffdbcccc6e1e8ee417b905acf983a
Dave Bush <github@david-bush.co.uk>
2016-02-29 21:41:26 +0000

    Added directory creation.

--- lib/creact.rb
+++ lib/creact.rb
@@ -2,11 +2,16 @@ require "creact/version"
 
 module Creact
   def self.create_dirs
+    FileUtils.cd '.'  #TODO: Update this to project dir.
+    dirs = ['views', 'js']
+    puts 'Creating the following dirs: ' + dirs.join(' ')
+    FileUtils.makedirs dirs
   end
 
   def self.create_views
   end
 
   def self.run_generator
+    self.create_dirs
   end
 end
