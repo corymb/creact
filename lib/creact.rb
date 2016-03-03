7ba505e8c1bf9034923d0bb75e1df19288c95088
Dave Bush <github@david-bush.co.uk>
2016-03-03 19:10:27 +0000

    Replicated dependency bug.

--- lib/creact.rb
+++ lib/creact.rb
@@ -1,15 +1,16 @@
 require "creact/version"
 require "creact/file_handlers"
+# If you uncomment this, it'll break:
+# require 'opal'
 
 
 module Creact
   def loader
-    opal = Opal::Server.new {|s|
-      s.append_path 'js'
-      s.main = 'app'
-    }
-    opal_boot_code = Opal::Processor.load_asset_code(opal.sprockets, 'react')
+    'Test string'
+    # opal = Opal::Server.new {|s|
+    #   s.append_path 'js'
+    #   s.main = 'app'
+    # }
+    # opal_boot_code = Opal::Processor.load_asset_code(opal.sprockets, 'react')
   end
-
-  
 end
