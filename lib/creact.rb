1c2b14c149a1311a59a81b917617a75bd7884740
Dave Bush <github@david-bush.co.uk>
2016-03-03 07:01:36 +0000

    Finished generating files.

--- lib/creact.rb
+++ lib/creact.rb
@@ -1,52 +1,51 @@
 require "creact/version"
 
 module Creact
+  def self.get_absolute_path(file)
+    location = File.dirname(__FILE__)
+    "#{location}/../files/#{file}"
+  end
+
   def self.create_dirs
-    FileUtils.cd Dir.pwd
     dirs = ['views', 'js']
     puts 'Creating the following dirs: ' + dirs.join(' ')
     FileUtils.makedirs dirs
   end
 
   def self.create_views(template=:mote)
-    FileUtils.cd Dir.pwd
     puts 'Creating view files...'
-    location = File.dirname(__FILE__)
+    # TODO: Refactor into a case statement:
     if template == :mote
-      @view_files = ['../files/layout.mote', '../files/home.mote']
-      files = @view_files.map { |file| "#{location}/#{file}" }
+      @view_files = ['layout.mote', 'home.mote']
     end
+    files = @view_files.map { |file| get_absolute_path(file) }
     FileUtils.cp(files, 'views')
   end
 
   def self.create_js(template=:mote)
-    FileUtils.cd Dir.pwd
     puts 'Creating js files...'
-    @js_files = ['files/jquery.js', 'files/reacting.rb']
+    @js_files = ['jquery.js', 'reacting.rb'].map { |file| get_absolute_path(file) }
     FileUtils.cp(@js_files, 'js')
   end
 
   def self.create_react_module
-    FileUtils.cd Dir.pwd
-    FileUtils.cp('files/react_helpers.rb', '.')
+    # TODO: Do I need this anymore?
+    FileUtils.cp(get_absolute_path('react_helpers.rb'), '.')
   end
 
   def self.creact_react_entry_point
-    FileUtils.cd Dir.pwd
-    FileUtils.cp('files/react.rb', 'js')
+    FileUtils.cp(get_absolute_path('react.rb'), 'js')
   end
 
   def self.check_dir_empty(dir)
-    FileUtils.cd Dir.pwd
     (Dir.entries(dir) - %w{ . .. }).empty?
   end
 
   def self.delete_files
-    FileUtils.cd Dir.pwd
     @js_files = ['js/jquery.js', 'js/reacting.rb']
     @view_files = ['views/layout.mote', 'views/home.mote']
     puts "Destructive operation: 'yes' to continue"
-    confirm = gets.strip
+    # confirm = gets.strip
 
     if confirm == 'yes'
       puts 'Deleting...'
@@ -67,8 +66,8 @@ module Creact
       if self.check_dir_empty('js')
         puts 'Deleting directory: js...'
         FileUtils.rm_rf 'js'
+        puts 'Done'
       end
-      puts 'Done'
     else
       puts 'Aborted.'
     end
@@ -77,8 +76,8 @@ module Creact
   def self.run_generator
     self.create_dirs
     self.create_views
-    # self.create_js
-    # self.create_react_module
-    # self.creact_react_entry_point
+    self.create_js
+    self.create_react_module
+    self.creact_react_entry_point
   end
 end
