0f9c61e3d7e456a3799ccb73ca13c8ea033f4a51
Dave Bush <github@david-bush.co.uk>
2016-03-04 07:23:33 +0000

    Moved files directory.

--- lib/creact.rb
+++ lib/creact.rb
@@ -4,11 +4,14 @@ require 'opal'
 
 module Creact
   def loader
-    'Test string'
-    # opal = Opal::Server.new {|s|
-    #   s.append_path 'js'
-    #   s.main = 'app'
-    # }
-    # opal_boot_code = Opal::Processor.load_asset_code(opal.sprockets, 'react')
+    opal = Opal::Server.new {|s|
+      s.append_path 'js'
+      s.main = 'app'
+    }
+    opal_boot_code = Opal::Processor.load_asset_code(opal.sprockets, 'react')
+    puts 1
+    puts opal
+    puts opal_boot_code
+    puts 2
   end
 end
