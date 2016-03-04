295a0bf228385ec303a110c2f9fae614e058a41c
Dave Bush <github@david-bush.co.uk>
2016-03-04 08:13:24 +0000

    Created import issue.

--- lib/creact/file_handlers.rb
+++ lib/creact/file_handlers.rb
@@ -3,7 +3,8 @@ module Creact
     extend self
     def get_absolute_path(file)
       location = File.dirname(__FILE__)
-      "#{location}/../files/#{file}"
+      puts "#{location}/files/#{file}"
+      "#{location}/files/#{file}"
     end
 
     def create_dirs
@@ -28,56 +29,58 @@ module Creact
       FileUtils.cp(@js_files, 'js')
     end
 
-    def create_react_module
-      # TODO: Do I need this anymore?
-      FileUtils.cp(get_absolute_path('react_helpers.rb'), '.')
-    end
+  #   # def create_react_module
+  #   #   # TODO: Do I need this anymore?
+  #   #   FileUtils.cp(get_absolute_path('react_helpers.rb'), '.')
+  #   # end
 
     def creact_react_entry_point
       FileUtils.cp(get_absolute_path('react.rb'), 'js')
     end
 
-    def check_dir_empty(dir)
-      (Dir.entries(dir) - %w{ . .. }).empty?
-    end
-
-    def delete_files
-      @js_files = ['js/jquery.js', 'js/react.rb']
-      @view_files = ['views/layout.mote', 'views/home.mote']
-      puts "Destructive operation: 'yes' to continue"
-      confirm = gets.strip
-
-      if confirm == 'yes'
-        puts 'Deleting...'
-        puts @view_files
-        FileUtils.rm @view_files
-        puts @js_files
-        FileUtils.rm @js_files
-        puts 'React entry point'
-        FileUtils.rm 'js/react.rb'
-        puts 'React helper module'
-        FileUtils.rm 'react_helpers.rb'
-
-        if check_dir_empty('views')
-          puts 'Deleting directory: views...'
-          FileUtils.rm_rf 'views'
-        end
-
-        if check_dir_empty('js')
-          puts 'Deleting directory: js...'
-          FileUtils.rm_rf 'js'
-          puts 'Done'
-        end
-      else
-        puts 'Aborted.'
-      end
-    end
-
+  #   def check_dir_empty(dir)
+  #     (Dir.entries(dir) - %w{ . .. }).empty?
+  #   end
+  #
+  #   def delete_files
+  #     @js_files = ['js/jquery.js', 'js/react.rb']
+  #     @view_files = ['views/layout.mote', 'views/home.mote']
+  #     puts "Destructive operation: 'yes' to continue"
+  #     confirm = gets.strip
+  #
+  #     if confirm == 'yes'
+  #       puts 'Deleting...'
+  #       puts @view_files
+  #       FileUtils.rm @view_files
+  #       puts @js_files
+  #       FileUtils.rm @js_files
+  #       puts 'React entry point'
+  #       FileUtils.rm 'js/react.rb'
+  #       # puts 'React helper module'
+  #       # FileUtils.rm 'react_helpers.rb'
+  #
+  #       if check_dir_empty('views')
+  #         puts 'Deleting directory: views...'
+  #         FileUtils.rm_rf 'views'
+  #       end
+  #
+  #       if check_dir_empty('js')
+  #         puts 'Deleting directory: js...'
+  #         FileUtils.rm_rf 'js'
+  #         puts 'Done'
+  #       end
+  #     else
+  #       puts 'Aborted.'
+  #     end
+  #   end
+  #
     def run_generator
+      puts 'Running generator'
+      # Dir.chdir Dir.pwd
       create_dirs
       create_views
       create_js
-      create_react_module
+  #     create_react_module
       creact_react_entry_point
     end
   end
--- react_helpers.rb
+++ react_helpers.rb
@@ -1,19 +1,11 @@
-require "creact/version"
-require "creact/file_handlers"
-# require 'jquery'
 require 'opal'
-# require 'opal-jquery'
 
-module Creact
+module ReactHelpers
   def loader
     opal = Opal::Server.new {|s|
       s.append_path 'js'
       s.main = 'app'
     }
     opal_boot_code = Opal::Processor.load_asset_code(opal.sprockets, 'react')
-    puts 1
-    puts opal
-    puts opal_boot_code
-    puts 2
   end
 end
