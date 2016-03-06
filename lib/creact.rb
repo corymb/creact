19e09edba6c8bb4d77baaba4ed11b1845de62f85
Dave Bush <github@david-bush.co.uk>
2016-03-06 15:36:43 +0000

    Removed commented out code.

--- lib/creact.rb
+++ lib/creact.rb
@@ -13,14 +13,4 @@ module Creact
     puts opal
     Opal::Sprockets.load_asset('react', opal.sprockets)
   end
-  #   opal = Opal::Server.new {|s|
-  #     s.append_path 'js'
-  #     s.main = 'app'
-  #   }
-  #   opal_boot_code = Opal::Processor.load_asset_code(opal.sprockets, 'react')
-  #   puts 1
-  #   puts opal
-  #   puts opal_boot_code
-  #   puts 2
-  # end
 end
