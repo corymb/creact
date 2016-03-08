15fe44846523d4a43db12d21f4dfa4210f9e919f
Dave Bush <github@david-bush.co.uk>
2016-03-08 08:01:23 +0000

    Stubbed file_operations tests.

---
+++ test/test_file_operations.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/creact.rb'
+
+class FileOperationsTest
+  # TODO: Bring file_handlers inline with file_operations
+  def test_get_absolute_path
+  end
+  def test_get_absolute_path
+  end
+  def test_create_dirs
+  end
+  def test_create_views
+  end
+  def test_create_js
+  end
+  def test_creact_react_entry_point
+  end
+  def test_run_generator
+  end
+end
