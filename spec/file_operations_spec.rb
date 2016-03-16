6dafb6b679affaba0a3ea348ea16ed6c725d5cbf
Dave Bush <github@david-bush.co.uk>
2016-03-16 22:46:52 +0000

    Added setup/teardown methods.

--- spec/file_operations_spec.rb
+++ spec/file_operations_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
 describe Creact::FileOperations do
+  after :each do
+    Creact::FileOperations.delete_dirs
+  end
+
   it 'creates js directory' do
     expect(Dir.exists? 'js').to be false
+    Creact::FileOperations.create_dirs
   end
   it 'creates css directory' do
     expect(Dir.exists? 'css').to be false
-    # Creact::FileOperations.create_dirs
+    Creact::FileOperations.create_dirs
   end
 end
