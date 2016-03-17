1d53b935a929c9777670c930ced1cb06cd7345cb
Dave Bush <github@david-bush.co.uk>
2016-03-17 06:18:17 +0000

    Added test to check directories are created.

--- spec/file_operations_spec.rb
+++ spec/file_operations_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Creact::FileOperations do
+
   after :each do
     Creact::FileOperations.delete_dirs
   end
@@ -8,9 +9,13 @@ describe Creact::FileOperations do
   it 'creates js directory' do
     expect(Dir.exists? 'js').to be false
     Creact::FileOperations.create_dirs
+    expect(Dir.exists? 'js').to be true
   end
+
   it 'creates css directory' do
     expect(Dir.exists? 'css').to be false
     Creact::FileOperations.create_dirs
+    expect(Dir.exists? 'js').to be true
   end
+
 end
